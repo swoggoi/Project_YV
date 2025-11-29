d5926397192f76846136ccd93ced3637491e602b
swoggoi <zxcasdqweff14@gmail.com>
2025-11-29 14:29:46 +0300

    test1

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+type User struct {
+	Username string
+	Name     string
+	Password string
+}
+
+func clearConsole() {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func (u *User) ChangePassword() bool {
+	var NewPassword string
+	fmt.Println("Введите новый пароль:")
+	fmt.Scan(&NewPassword)
+	if u.Password == NewPassword {
+		fmt.Println("Пароли одинаковые")
+		return false
+	}
+	u.Password = NewPassword
+	fmt.Println("Вы успешно сменили пароль!")
+	return true
+}
+
+func (u *User) ChangeUsername() bool {
+	var NewUsername string
+	fmt.Println("Введите новый username:")
+	fmt.Scan(&NewUsername)
+	if u.Username == NewUsername {
+		fmt.Println("user'ы одинаковые")
+		return false
+	}
+	u.Username = NewUsername
+	fmt.Println("Вы успешно сменили username!")
+	return true
+}
+
+func main() {
+	NewUser := User{
+		Username: "@sanya",
+		Name:     "Sasha",
+		Password: "Arina1978!",
+	}
+
+	fmt.Println("---------------------------")
+	fmt.Println("| ПРИВЕТСТВУЕМ ВАС", NewUser.Name, "  |")
+	fmt.Println("| ваш username:", NewUser.Username, " |")
+	fmt.Println("| пароль:", NewUser.Password, "       |")
+	fmt.Println("---------------------------")
+	NewUser.ChangePassword()
+	clearConsole()
+	NewUser.ChangeUsername()
+	clearConsole()
+	fmt.Println("---------------------------")
+	fmt.Printf("| %-20s |\n", "ПРИВЕТСТВУЕМ ВАС "+NewUser.Name)
+	fmt.Printf("| %-20s |\n", "ваш username: @"+NewUser.Username)
+	fmt.Printf("| %-20s |\n", "пароль: "+NewUser.Password)
+
+	fmt.Println("---------------------------")
+
+}
