5ee5a781ceb681e4d31c3d05edf93fabdf2fc9e1
swoggoi <zxcasdqweff14@gmail.com>
2025-12-01 16:29:40 +0300

    asdadasd

--- db.go
+++ db.go
@@ -7,13 +7,16 @@ import (
 )
 
 func initDB() *sql.DB {
+
 	connStr := "host=localhost port=5432 user=postgres password=123123 dbname=postgres sslmode=disable"
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
 	}
 	return db
 }
+
 func updatePassword(db *sql.DB, username, newPassword string) error {
 	_, err := db.Exec(`UPDATE users SET password = $1 WHERE username = $2`, newPassword, username)
 	return err
